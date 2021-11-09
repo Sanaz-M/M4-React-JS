78f155159e3727cbf13fad09fd12fa9def8ae91d
Sanaz M <sanazmohammaddi@gmail.com>
2021-11-09 16:49:04 +0000

    First Reach App

--- strive-library/src/components/LatestRelease.jsx
+++ strive-library/src/components/LatestRelease.jsx
@@ -1,15 +1,25 @@
-const LatestRelease = () => (
-    <div>
-        <container> 
-            <row className="justify-content-center"> {
-                categoryScifi.map((book) => {
+import { Container, Col, Row, Card, CardImg } from 'react-bootstrap'
+import scifiBooks from '../data/scifi.json'
+import { Component } from 'react'
 
-                })
-            }
+class LatestRelease extends Component {
+    render() {
+        return (
+        <Container> 
+            <Row className="justify-content-center"> {
+                scifiBooks.map((book => (
+                    <Col md={2} key = {book.asin}>
+                        <Card>
+                        <CardImg src = {book.img}></CardImg>
+                        </Card>
+                        </Col>
+                )
+                )
+                )} 
             
-            </row> 
-        </container>
-    </div>
-)
-
+            </Row> 
+        </Container>
+        )
+    }
+}
 export default LatestRelease
