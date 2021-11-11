cfdc61d1ad27caa9fce285c23bf02ce373b10738
Sanaz M <sanazmohammaddi@gmail.com>
2021-11-11 16:11:01 +0000

    updated

--- strive-library/src/components/SingleBook.jsx
+++ strive-library/src/components/SingleBook.jsx
@@ -42,32 +42,6 @@ class SingleBook extends Component {
         }
       }
 
-
-
-      getOthersComment = async () => {
-        try {
-          const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + this.props.elementId ,{
-              methode: 'GET',
-              body: JSON.stringify(this.state.yourComment),
-              headers: {
-                'Content-type': 'application/json',
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGJmM2FhY2FhMjAwMTU1MmExNmQiLCJpYXQiOjE2MzU5NDU0NTksImV4cCI6MTYzNzE1NTA1OX0.68CC8Jf4IHn7VZW39FPf-bHEv8MKux00DbaR2yT026Y"
-              }
-          })
-          if(response.ok) {
-            const data = await response.json()
-              this.setState({ yourComment: data })
-          } else {
-              console.log('error while fetching')
-          }
-        } catch(e) {
-          console.log(e)
-        }
-      }
-
-
-
-
     render() {
         return (
             <Row>
